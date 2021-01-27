59b28a1d859e59b82391340b98088f6a6c94de74
MARO DE MELO <marodemelo10@gmail.com>
2021-01-27 19:30:04 -0300

    Criado layout do menu do sistema

---
+++ Sistema de Vendas/Models/ClienteModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Sistema_de_Vendas.Models
+{
+    public class ClienteModel
+    {
+    }
+}
