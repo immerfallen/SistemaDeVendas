c36174dcd61aff2f51c528802e39ea3863c64ef8
MARO DE MELO <marodemelo10@gmail.com>
2021-01-27 20:22:06 -0300

    Listagem de clientes

--- Sistema de Vendas/Controllers/ClienteController.cs
+++ Sistema de Vendas/Controllers/ClienteController.cs
@@ -3,6 +3,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Sistema_de_Vendas.Models;
 
 namespace Sistema_de_Vendas.Controllers
 {
@@ -10,6 +11,9 @@ namespace Sistema_de_Vendas.Controllers
     {
         public IActionResult Index()
         {
+
+            ViewBag.ListaClientes = new ClienteModel().ListarTodosClientes();
+
             return View();
         }
     }
--- Sistema de Vendas/Models/ClienteModel.cs
+++ Sistema de Vendas/Models/ClienteModel.cs
@@ -2,10 +2,44 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using Sistema_de_Vendas.Uteis;
+using System.Data;
 
 namespace Sistema_de_Vendas.Models
 {
     public class ClienteModel
     {
+        public string Id { get; set; }
+        public string Nome { get; set; }
+        public string CPF_CNPJ { get; set; }
+        public string  Email { get; set; }
+        public string Senha { get; set; }
+
+
+
+        public List<ClienteModel> ListarTodosClientes()
+        {
+            List<ClienteModel> lista = new List<ClienteModel>();
+            ClienteModel item;
+            DAL objDAL = new DAL();
+            string sql = "SELECT id, nome, cpf_cnpj, email, senha FROM cliente ORDER BY nome ASC";
+            DataTable dt = objDAL.RetDataTable(sql);
+
+            for (int i = 0; i < dt.Rows.Count; i++)
+            {
+                item = new ClienteModel()
+                {
+                    Id = dt.Rows[i]["Id"].ToString(),
+                    Nome = dt.Rows[i]["Nome"].ToString(),
+                    CPF_CNPJ = dt.Rows[i]["CPF_CNPJ"].ToString(),
+                    Email = dt.Rows[i]["Email"].ToString(),
+                    Senha = dt.Rows[i]["Senha"].ToString()
+                };
+                lista.Add(item);
+
+            }
+            return lista;
+        }
     }
 }
