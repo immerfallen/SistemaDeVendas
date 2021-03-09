8c9d4d1dc104103de8db46cf83cfedad649f6176
Maro de Melo <marodemelo10@gmail.com>
2021-03-08 21:19:30 -0300

    Alterado método ValidarLogin da classe LoginModel para impedir SQL Injection nos campos do formulário

--- Sistema de Vendas/Models/LoginModel.cs
+++ Sistema de Vendas/Models/LoginModel.cs
@@ -5,6 +5,7 @@ using System.Data;
 using System.Linq;
 using System.Threading.Tasks;
 using System.ComponentModel.DataAnnotations;
+using MySql.Data.MySqlClient;
 
 namespace Sistema_de_Vendas.Models
 {
@@ -21,14 +22,19 @@ namespace Sistema_de_Vendas.Models
         public string Senha { get; set; }
 
 
-        // Existe um problema grave de segurança com essa abordagem => SQL Injection
+       
         public bool ValidarLogin()
         {
-            string sql = $"SELECT ID, NOME FROM VENDEDOR WHERE EMAIL='{Email}' AND SENHA='{Senha}'";
-            MySqlCommand Command = 
+            string sql = $"SELECT ID, NOME FROM VENDEDOR WHERE email=@email AND senha=@senha";
+            MySqlCommand Command = new MySqlCommand();
+            Command.CommandText = sql;
+            Command.Parameters.AddWithValue("@email", Email);
+            Command.Parameters.AddWithValue("@senha", Senha);
+
             DAL objDAL = new DAL();
-            DataTable dt = objDAL.RetDataTable(sql);
-            if (dt.Rows.Count>=1)
+
+            DataTable dt = objDAL.RetDataTable(Command);
+            if (dt.Rows.Count==1)
             {
                 Id = dt.Rows[0]["Id"].ToString();
                 Nome = dt.Rows[0]["Nome"].ToString();
